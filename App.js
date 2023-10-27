0614579400ac58188f0bc446232ff2f521af110f
Salim Ali <u14262208@tuks.co.za>
2023-10-27 05:04:55 +0200

    implemented pie chart component

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { SafeAreaView, View, StatusBar, ScrollView } from "react-native";
 import * as constants from "./constants";
 import GlassCard from "./components/GlassCard";
 import GradientCard from "./components/GradientCard";
+import DonutChart from "./components/DonutChart";
 import { styles } from "./styles";
 
 
@@ -9,11 +10,11 @@ export default function App() {
 
   return (
 
-    <SafeAreaView style = {{ backgroundColor: constants.dark, flex: 1 }}>
+    <SafeAreaView style = {{ backgroundColor: constants.lunar, flex: 1 }}>
 
       <ScrollView>
 
-        <StatusBar backgroundColor={constants.dark}
+        <StatusBar backgroundColor={constants.lunar}
                    barStyle="light-content" />
 
         <View style={styles.cardSection}>
@@ -25,6 +26,8 @@ export default function App() {
           <GradientCard />
 
         </View>
+
+        <DonutChart data={[40, 90, 27]} />
 
       </ScrollView>
 
--- constants.js
+++ constants.js
@@ -1,7 +1,23 @@
 
+export const granite = "#1e1e1e";
+export const lunar = "#222222";
+
 export const navy = "#302d43";
 export const blue = "#6953f7";
 export const purple = "#cd4ff7";
 export const dark = "#22202d";
 export const screen = "#322f46";
 export const peach = "#f9af6c";
+export const green = "#86dc3d";
+export const pink = "#ff6ec7";
+export const orange = "#f28500";
+// export const orange = "#ff7518";
+export const yellow = "#fee12b";
+export const red = "#dc143c";
+export const indigo = "#8a2be2";
+export const neon = "#7b00ff";
+export const violet = "#9400d3";
+export const electric = "#8f00ff";
+
+// export const colours = [blue, purple, green, orange, pink, indigo, red, violet, neon, yellow, electric, peach];
+export const colours = [blue, purple, green, orange, pink, red, yellow, peach, indigo, violet, neon, electric];
