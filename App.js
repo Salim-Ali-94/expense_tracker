142d96d06beca406c780416b92d51161f6ce1058
Salim Ali <u14262208@tuks.co.za>
2023-10-30 12:04:39 +0200

    converted the progress indicator into a separate component

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-// import { SafeAreaView, Pressable, Image, Text, View, StatusBar, ScrollView } from "react-native";
 import { SafeAreaView, Image, Text, View, StatusBar, ScrollView } from "react-native";
 import * as constants from "./constants";
 import GlassCard from "./components/GlassCard";
@@ -6,7 +5,8 @@ import GradientCard from "./components/GradientCard";
 // import DonutChart from "./components/DonutChart";
 import CategoryCard from "./components/CategoryCard";
 import { styles } from "./styles";
-import { ProgressCircle } from "react-native-svg-charts";
+import ProgressIndicator from "./components/ProgressIndicator";
+
 
 export default function App() {
 
@@ -79,21 +79,9 @@ export default function App() {
 
           <View style={{width: "92%"}}>
 
-            <View style={{width: 80, height: 80, justifyContent: "center", alignItems: "center"}}>
-
-              <ProgressCircle style={{ width: 80, height: 80 }}
-                              progress={0.8}
-                              progressColor={"rgb(134, 65, 244)"}
-                              backgroundColor={"transparent"}
-                              startAngle={-Math.PI*0}
-                              cornerRadius={1}
-                              strokeWidth={3} />
-                            {/* endAngle={Math.PI} >  */}
-
-              <Image source={constants.icons.travel}
-                    style={{width: 40, height: 40, zIndex: 2, position: "absolute"}} />
-
-            </View>
+            <ProgressIndicator tint={constants.purple}
+                               percent={0.6}
+                               icon={constants.icons.travel} />
 
           </View>
 
---
+++ components/ProgressIndicator/styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+
+export const styles = StyleSheet.create({
+
+    progressBox: {
+        
+        width: 80,
+        height: 80,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+
+    circle: {
+        
+        width: 80,
+        height: 80
+    },
+
+    imageBox: {
+        
+        width: 40,
+        height: 40,
+        position: "absolute"
+    }
+
+});
